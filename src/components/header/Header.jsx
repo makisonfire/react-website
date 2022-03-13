c0920655e5de0dd3dce39324636aa3e128ebfc80
makisonfire <k.makihashi@gmial.com>
2022-03-13 20:02:54 +0900

    installed react icons

--- src/components/header/Header.jsx
+++ src/components/header/Header.jsx
@@ -1,9 +1,26 @@
 import React from 'react'
 import './header.css'
+import CTA from './CTA'
+import HeaderSocials from './HeaderSocials'
+import ME from '../../assets/me.png'
 
 const Header = () => {
   return (
-    <div>Header</div>
+    <header>
+      <div className="container header__container">
+        <h5>Hello I'm</h5>
+        <h1>Katsuhiro Makihashi</h1>
+        <h5 className="text-light">Frontend Engineer</h5>
+        <CTA />
+
+        <div className="me">
+          <img src={ME} alt="ME" />
+          <a href="#contact" className="scroll__down">Scroll Down</a>
+          <HeaderSocials />
+        </div>
+      </div>
+    </header>
+
   )
 }
 
