5da2c16915ff711a93149052861ac8131957177b
makisonfire <k.makihashi@gmial.com>
2022-03-21 21:59:11 +0900

    Dynamically generated portfolio component

---
+++ src/components/portfolio/Portfolio.jsx
@@ -0,0 +1,85 @@
+import React from 'react'
+import './portfolio.css'
+import IMG1 from '../../assets/portfolio1.jpg'
+import IMG2 from '../../assets/portfolio2.jpg'
+import IMG3 from '../../assets/portfolio3.jpg'
+import IMG4 from '../../assets/portfolio4.jpg'
+import IMG5 from '../../assets/portfolio5.png'
+import IMG6 from '../../assets/portfolio6.jpg'
+
+const data = [
+    {
+        id: 1,
+        image: IMG1,
+        title: 'Cyrpto Currency Dashboard & Financial Visualization',
+        github: 'https://github.com',
+        demo: 'https://dribble.com'
+    },
+    {
+        id: 2,
+        image: IMG2,
+        title: 'Cyrpto Currency Dashboard & Financial Visualization',
+        github: 'https://github.com',
+        demo: 'https://dribble.com'
+    },
+    {
+        id: 3,
+        image: IMG3,
+        title: 'Cyrpto Currency Dashboard & Financial Visualization',
+        github: 'https://github.com',
+        demo: 'https://dribble.com'
+    },
+    {
+        id: 4,
+        image: IMG4,
+        title: 'Cyrpto Currency Dashboard & Financial Visualization',
+        github: 'https://github.com',
+        demo: 'https://dribble.com'
+    },
+    {
+        id: 5,
+        image: IMG5,
+        title: 'Cyrpto Currency Dashboard & Financial Visualization',
+        github: 'https://github.com',
+        demo: 'https://dribble.com'
+    },
+    {
+        id: 6,
+        image: IMG6,
+        title: 'Cyrpto Currency Dashboard & Financial Visualization',
+        github: 'https://github.com',
+        demo: 'https://dribble.com'
+    }
+];
+
+
+const Portfolio = () => {
+    return (
+      <section id="portfolio">
+        <h5>My Recent Works</h5>
+        <h2>My Portfolio</h2>
+  
+       <div className="container portfolio__container">
+           {
+               data.map(({id,image,title,github,demo}) => {
+                return (
+                    <article key={id} className="portfolio__item">
+                        <div className="portfolio__item-image">
+                            <img src={image} alt={title} />
+                        </div>
+                        <h3>{title}</h3>
+                        <div className="portfolio__item-cta">
+                            <a rel="noreferrer" target="_blank" href={github} className="btn">Github</a>
+                            <a rel="noreferrer" target="_blank" href={demo} className="btn btn-primary">Live Demo</a>
+                        </div>
+                    </article>
+                )
+               }
+             )
+            }
+       </div>
+      </section>
+    )
+  }
+  
+  export default Portfolio
