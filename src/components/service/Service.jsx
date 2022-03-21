c8ba3c61676fa2453e198518dce3d0fe50a4eb8e
makisonfire <k.makihashi@gmial.com>
2022-03-21 17:16:45 +0900

    Renamed 'Works' as 'Service'

---
+++ src/components/service/Service.jsx
@@ -0,0 +1,114 @@
+import React from 'react'
+import './service.css'
+import {FiCheck} from 'react-icons/fi'
+
+const Service = () => {
+  return (
+    <section id="works">
+      <h5>What I Offer</h5>
+      <h2>Services</h2>
+
+      <div className="container services__container">
+        <article className="service">
+          <div className="service__head">
+            <h3>UI/UX</h3>
+          </div>
+
+          <ul className="service__list">
+            <li>
+              <FiCheck className="service__list-icon" />
+              <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. </p>
+            </li>
+            <li>
+              <FiCheck className="service__list-icon" />
+              <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. </p>
+            </li>
+            <li>
+              <FiCheck className="service__list-icon" />
+              <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. </p>
+            </li>
+            <li>
+              <FiCheck className="service__list-icon" />
+              <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. </p>
+            </li>
+            <li>
+              <FiCheck className="service__list-icon" />
+              <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. </p>
+            </li>
+          </ul>
+        </article>
+        {/* End of UI/UX */}
+
+        <article className="service">
+          <div className="service__head">
+            <h3>Web Development</h3>
+          </div>
+
+          <ul className="service__list">
+            <li>
+              <FiCheck className="service__list-icon" />
+              <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. </p>
+            </li>
+            <li>
+              <FiCheck className="service__list-icon" />
+              <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. </p>
+            </li>
+            <li>
+              <FiCheck className="service__list-icon" />
+              <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. </p>
+            </li>
+            <li>
+              <FiCheck className="service__list-icon" />
+              <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. </p>
+            </li>
+            <li>
+              <FiCheck className="service__list-icon" />
+              <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. </p>
+            </li>
+            <li>
+              <FiCheck className="service__list-icon" />
+              <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. </p>
+            </li>
+          </ul>
+        </article>
+        {/* End of Web Development */}
+
+        <article className="service">
+          <div className="service__head">
+            <h3>Content Creation</h3>
+          </div>
+
+          <ul className="service__list">
+            <li>
+              <FiCheck className="service__list-icon" />
+              <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. </p>
+            </li>
+            <li>
+              <FiCheck className="service__list-icon" />
+              <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. </p>
+            </li>
+            <li>
+              <FiCheck className="service__list-icon" />
+              <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. </p>
+            </li>
+            <li>
+              <FiCheck className="service__list-icon" />
+              <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. </p>
+            </li>
+            <li>
+              <FiCheck className="service__list-icon" />
+              <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. </p>
+            </li>
+            <li>
+              <FiCheck className="service__list-icon" />
+              <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. </p>
+            </li>
+          </ul>
+        </article>
+        {/* End of Content Creation */}
+      </div>
+    </section>
+  )
+}
+
+export default Service
