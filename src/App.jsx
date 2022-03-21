c9495e36fef22c081b2f3bf8efcc524e2d99efea
makisonfire <k.makihashi@gmial.com>
2022-03-21 21:59:42 +0900

    renamed 'works' as 'service'

--- src/App.jsx
+++ src/App.jsx
@@ -2,10 +2,11 @@ import React from 'react'
 import Header from './components/header/Header'
 import About from './components/about/About'
 import Experience from './components/experience/Experience'
-import Works from './components/works/Works'
+import Service from './components/service/Service'
 import Nav from './components/nav/Nav'
 import Contact from './components/contact/Contact'
 import Footer from './components/footer/Footer'
+import Portfolio from './components/portfolio/Portfolio'
 
 const App = () => {
   return (
@@ -14,7 +15,8 @@ const App = () => {
       <Nav />
       <About />
       <Experience />
-      <Works />
+      <Service />
+      <Portfolio />
       <Contact />
       <Footer />
     </>
